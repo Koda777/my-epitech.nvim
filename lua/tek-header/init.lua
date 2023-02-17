27fb9308b271cdabb6d70b67a3bde13fed362c16
koda <alexandre.decobert@epitech.eu>
2023-02-17 23:18:52 +0100

    [ADD] header cpp

--- lua/tek-header/init.lua
+++ lua/tek-header/init.lua
@@ -8,44 +8,58 @@
 -- set shortcut
 vim.api.nvim_set_keymap("n", "<leader>h", ":lua Header()<CR>", { noremap = true, silent = false })
 
+local file_name = vim.fn.expand("%:t")
+
 -- get project name
-function GetProjetName()
+local project_name = function()
 	vim.api.nvim_command("call inputsave()")
-	local project_name = vim.fn.input("Project name: ")
-	if project_name == "" then
-		print("Project name generated from current directory. If you want to change it, please use <leader>h")
-		project_name = vim.fn.split(vim.fn.getcwd(), "/")[#vim.fn.split(vim.fn.getcwd(), "/")]
+	local name = vim.fn.input("Project name: ")
+	if name == "" then
+		-- print("Project name generated from current directory. If you want to change it, please use <leader>h")
+		name = vim.fn.split(vim.fn.getcwd(), "/")[#vim.fn.split(vim.fn.getcwd(), "/")]
 	end
-	return project_name
+	return name
 end
 
 -- get project description
-function GetProjetDescription(file_name)
+local project_description = function()
 	vim.api.nvim_command("call inputsave()")
-	local project_description = vim.fn.input("Project description: ")
-	if project_description == "" then
-		print("Project description generated from the file name. If you want to change it, please use <leader>h")
-	end
+	local description = vim.fn.input("Project description: ")
 
-	return (project_description == "" and file_name or project_description)
+	return (description == "" and file_name or description)
 end
 
-local function addHeader()
-	-- Get the current buffer handle
+-- generate header
+local header = function()
+	local header = string.format(
+		"/*\n** EPITECH PROJECT, %d\n** %s\n** File description:\n** %s\n*/",
+		os.date("%Y"),
+		project_name(),
+		project_description()
+	)
+	return header
+end
+
+local header_line = function()
+	local header_lines = {}
+	for line in string.gmatch(header(), "[^\n]+") do
+		table.insert(header_lines, line)
+	end
+	return header_lines
+end
+
+-- set header style
+local HeaderStyle = function(index, line)
 	local buf = vim.api.nvim_get_current_buf()
-	-- Save the current cursor position
 	local cursor = vim.api.nvim_win_get_cursor(0)
-
-	-- Go to the top of the buffer and insert the header
-	vim.api.nvim_buf_set_lines(buf, 0, 0, false, { "/////////////// salut" })
-
-	-- Restore the cursor position
+	vim.api.nvim_buf_set_lines(buf, index, index, false, { line })
 	vim.api.nvim_win_set_cursor(0, cursor)
 end
 
 -- set header
 function Header()
-	local file_name = vim.fn.expand("%:t")
-	local project_name = GetProjetName()
-	local project_description = GetProjetDescription(file_name)
+	local header_lines = header_line()
+	for i, line in ipairs(header_lines) do
+		HeaderStyle(i - 1, line)
+	end
 end
